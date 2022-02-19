99b9b6ce9fa31ec67eb8c1338f456efb00c44bc1
jamessccho <james.sungchul.cho@gmail.com>
2022-02-19 12:19:17 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS DEMO Blog 1
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
